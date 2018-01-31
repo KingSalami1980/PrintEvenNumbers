e64d20297cc00dc167111aa1331d4c22d98f6526
Clive Harding <35668864+KingSalami1980@users.noreply.github.com>
2018-01-31 06:46:50 +0200

    Add files via upload

---
+++ ConsoleAppPrintEvenNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleAppPrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 96, 23 };
+
+            var thatsEven = numbers.Where(n => n % 2 == 0);
+
+            foreach (int i in thatsEven)
+            {
+                Console.WriteLine(i.ToString());
+            }
+
+            Console.ReadLine();
+        }        
+    }
+}
